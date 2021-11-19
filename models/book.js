bac0ae639309a66f21da671a04d1b8684e945dd7
dilusha07 <dilushasandaruwani07@gmail.com>
2021-11-19 22:39:34 +0530

    Create the book schema

---
+++ models/book.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  isAvailable: {
+    type: Boolean,
+  },
+  burrowedmemberId: {
+    type: String,
+  },
+  burrowedDate: {
+    type: String,
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
